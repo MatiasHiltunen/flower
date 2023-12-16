f584b0366b394fbcd79a6a8dc31a2ce95f160849
MatiasHiltunen <matias.s.j@hotmail.com>
2023-12-16 23:20:34 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Canvas } from '@react-three/fiber'
+import { CameraControls, Environment, MeshTransmissionMaterial } from '@react-three/drei'
+import { Splat } from './splat'
+
+export default function App() {
+  return (
+    <Canvas dpr={[1, 1.5]} camera={{ position: [0, 6, -1], fov: 50 }}>
+      <color attach="background" args={['#151520']} />
+      <Splat scale={6} rotation={[0, Math.PI, 0]} src="hyasintti.splat" />
+
+      <Environment preset="apartment" />
+      <CameraControls />
+    </Canvas>
+  )
+}
