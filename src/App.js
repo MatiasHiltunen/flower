d64ee1410c747bf9702626180898d71b3c4d1733
MatiasHiltunen <matias.s.j@hotmail.com>
2023-12-18 10:37:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
     <Canvas dpr={[1, 1.5]} camera={{ position: [0, 6, -1], fov: 50 }}>
       <color attach="background" args={['#151520']} />
       <Splat scale={6} rotation={[0, Math.PI, 0]} src="hyasintti.splat" />
+  <Splat scale={6} position={[3,0,0]} rotation={[0, Math.PI, 0]} src="hyasintti.splat" />
 
       <Environment preset="apartment" />
       <CameraControls />
